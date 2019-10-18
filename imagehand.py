1c2f713b2e9ee55ab86a19ca5287734673113d2c
SaiTeja69 <54733232+SaiTeja69@users.noreply.github.com>
2019-10-18 22:37:09 +0530

    Add files via upload

---
+++ imagehand.py
@@ -0,0 +1,45 @@
+import numpy as np
+from PIL import Image
+
+def make_image(list1,count):
+ imgs    = [ Image.open(i) for i in list1 ]
+ #min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
+ min_shape=(10*y,10*y)
+ imgs_comb = np.hstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
+ imgs_comb = Image.fromarray( imgs_comb)
+ imgs_comb.save( str(count)+'.jpg' )  
+def multiple_lines(count):
+ list1=[]
+ for i in range(1,count+2,1):
+  z=str(i)+".jpg"
+  list1.append(z)
+ imgs    = [ Image.open(i) for i in list1 ]
+ min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
+# min_shape=(300*y,200*y)
+ imgs_comb = np.vstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
+ imgs_comb = Image.fromarray( imgs_comb)
+ imgs_comb.save( 'final.jpg')
+ 
+#list_im = ['Test1.jpg', 'Test2.jpg', 'Test3.jpg']
+x=open("new.txt")
+list1=[]
+listx=[i for i in x.read()]
+y=0
+count=0
+for i in listx:
+ if i=='\n':
+  count=count+1
+  if(len(list1)<50):
+   for i in range(len(list1),50,1):
+    list1.append('space.jpg')
+  make_image(list1,count)
+  list1.clear()
+  continue
+ if(i>='a' and i<= 'z') :
+  z=i+".jpg"
+ else:
+  z='space.jpg'
+ list1.append(z)
+ y=y+1
+if(count>0):
+ multiple_lines(count)
